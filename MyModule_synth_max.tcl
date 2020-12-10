73b42d12bd1df2e3c4b2a7408a5b16967e8fa493
Alex-sqrt <75654963+Alex-sqrt@users.noreply.github.com>
2020-12-10 10:33:12 +0300

    Synthesisi
    Файл, который содержит команды для RTL_Compiler.
    Случай быстрого корнера

--- MyModule_synth_max.tcl
+++ MyModule_synth_max.tcl
@@ -1,15 +1,15 @@
 ## Setup technology files
-include ../Sourse/X-FAB_max.tcl
+include ../Source/X-FAB_max.tcl
 ## Setup variables
 set DESIGN Sqrt2
 set PARAMS {}
 ## Read in Verilog HDL files
 # Top module
-read_hdl -v2001 ../Sourse/Sqrt2/Sqrt2.v
+read_hdl -v2001 ../Source/Sqrt2.v
 ## Compile our code (create a technology-independent schematic)
 elaborate -parameters $PARAMS $DESIGN
 ## Setup design constraints
-read_sdc ../Sourse/Sqrt2/Synthesis/Sqrt2.sdc
+read_sdc ../Source/Sqrt2.sdc
 ## Synthesize our schematic (create a technology-dependent schematic)
 #synthesize -to_generic
 synthesize -to_mapped
@@ -19,8 +19,8 @@ synthesize -incremental
 report area > ../Reports/Sqrt2_synth_area_report_max
 report timing > ../Reports/Sqrt2_synth_timing_report_max
 ## Write out synthesized Verilog netlist
-##write_hdl -mapped > ../Sourse/Sqrt2/Synthesis/Sqrt2_synth_max.v
+##write_hdl -mapped > ../Source/Synthesis/Sqrt2_synth_max.v
 ## Write out the SDC file we will take into the place n route tool
-##write_sdc > ../Sourse/Sqrt2/Synthesis/Sqrt2_out_max.sdc
+##write_sdc > ../Source/Synthesis/Sqrt2_out_max.sdc
 gui_show
 
