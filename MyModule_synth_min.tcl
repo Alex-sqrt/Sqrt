9e9be01f4309d12c56331d602c44c9ea659be522
Alex-sqrt <75654963+Alex-sqrt@users.noreply.github.com>
2020-12-10 10:34:50 +0300

    Synthesis
    Файл с командами для RTL_Compiler
    Случай медленного корнера

--- MyModule_synth_min.tcl
+++ MyModule_synth_min.tcl
@@ -1,15 +1,15 @@
 ## Setup technology files
-include ../Sourse/X-FAB_min.tcl
+include ../Source/X-FAB_min.tcl
 ## Setup variables
 set DESIGN Sqrt2
 set PARAMS {}
 ## Read in Verilog HDL files
 # Top module
-read_hdl -v2001 ../Sourse/Sqrt2/Sqrt2.v
+read_hdl -v2001 ../Source/Sqrt2.v
 ## Compile our code (create a technology-independent schematic)
 elaborate -parameters $PARAMS $DESIGN
 ## Setup design constraints
-read_sdc ../Sourse/Sqrt2/Synthesis/Sqrt2.sdc
+read_sdc ../Source/Synthesis/Sqrt2.sdc
 ## Synthesize our schematic (create a technology-dependent schematic)
 #synthesize -to_generic
 synthesize -to_mapped
@@ -19,8 +19,8 @@ synthesize -incremental
 report area > ../Reports/Sqrt2_synth_area_report_min
 report timing > ../Reports/Sqrt2_synth_timing_report_min
 ## Write out synthesized Verilog netlist
-##write_hdl -mapped > ../Sourse/Sqrt2/Synthesis/Sqrt2_synth_min.v
+##write_hdl -mapped > ../Source/Synthesis/Sqrt2_synth_min.v
 ## Write out the SDC file we will take into the place n route tool
-##write_sdc > ../Sourse/Sqrt2/Synthesis/Sqrt2_out_min.sdc
+##write_sdc > ../Source/Synthesis/Sqrt2_out_min.sdc
 gui_show
 
